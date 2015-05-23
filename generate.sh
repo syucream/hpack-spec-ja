4278304234cb1b36c0bc62222cc9e4527666e5de
Ryo Okubo <syucream1031@gmail.com>
2015-05-23 19:58:35 +0900

    Improve generation script

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-target="header-compression-10-ja"
+target=$1
 style="rfc2629.xslt"
 
-java -classpath http2-spec/lib/saxon9.jar net.sf.saxon.Transform -novw -l "draft-ietf-httpbis-${target}.xml" $style | sed -f http2-spec/lib/addstyle.sed > "${target}.html"
+java -classpath http2-spec/lib/saxon9.jar net.sf.saxon.Transform -novw -l $target $style | sed -f http2-spec/lib/addstyle.sed
