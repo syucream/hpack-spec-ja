a88e3d4723ed4104ed82391cb38d2fbc692a7b62
Ryo Okubo <syucream1031@gmail.com>
2015-07-08 23:36:32 +0900

    Fix not to remove necessary spaces

--- generate.sh
+++ generate.sh
@@ -1,7 +1,8 @@
 # $ sh ./generate.sh [translated xml]
 
 target=$1
-style="rfc2629.xslt"
+xslt="rfc2629.xslt"
 
-# Format translated xml and convert xml to html by using rfc2629.xslt
-perl -pe "s/^\s+//g" $1 | perl -pe "s/([^\x01-\x7E])\n/\$1/g" | java -classpath http2-spec/lib/saxon9.jar net.sf.saxon.Transform -novw -l /dev/stdin $style | sed -f http2-spec/lib/addstyle.sed
+# Format translated xml and convert it to html by rfc2629.xslt
+# 1) Eliminate unnecessary whitespaces 2) Build html by xslt 3) Apply modification relating to styles
+perl -pe "s/^\s+([^\x01-\x7E])/\$1/g" $1 | perl -pe "s/([^\x01-\x7E])\n/\$1/g" | java -classpath http2-spec/lib/saxon9.jar net.sf.saxon.Transform -novw -l /dev/stdin $xslt | sed -f http2-spec/lib/addstyle.sed
